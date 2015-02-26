e3fa29aa702963e13f4e4b51813efdc9e68781d0
Jason Matthews <jmatty1983@gmail.com>
2015-02-26 21:50:05 +0000

    Add db seed and Event Attendance

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'faker'
 gem 'bcrypt'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.6'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(username: 'jason',
+             password: 'password',
+             password_confirmation: 'password')
+             
+50.times do |x|
+    name = Faker::Name.first_name
+    password = password_confirmation = 'password'
+    User.create!(username: name,
+                 password: password,
+                 password_confirmation: password_confirmation)
+end
+
+users = User.order(:created_at).take(6)
+12.times do |x|
+    users.each do |user|
+       name = "#{Faker::App.name} #{Faker::App.name}"
+       date = Faker::Time.between(1.year.ago, 1.year.from_now)
+       event = user.events.create!(name: name, date: date)
+       attendees = User.all.sample(10)
+       event.attendees.concat(attendees)
+    end
+end
